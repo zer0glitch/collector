c2433b649f7f20290195d62ffa096e1f4dd9bec2
usrbinkat <usrbinkat@braincraft.io>
2021-03-31 13:13:41 -0700

    add ibmcloud and rhcos images and fix wall-e oc binary path issue
    Signed-off-by: usrbinkat <usrbinkat@braincraft.io>

--- hack/publish.sh
+++ hack/publish.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
-rm -rf $(ls galaxy/*)
+rm -rf $(ls galaxy/* 2>/dev/null)
 ansible-galaxy collection build --output-path galaxy
-ansible-galaxy collection publish --token="$1" $(ls galaxy/*)
+ansible-galaxy collection publish --api-key="$1" $(ls galaxy/*)
