3242a4c38654764c0ea6c4d4e6484175c1ac18c6
root <root@smuggler.aws-dev20.acs.sd.spawar.navy.mil>
2021-04-20 18:52:00 -0700

    pushing

--- hack/publish.sh
+++ hack/publish.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
 rm -rf $(ls galaxy/* 2>/dev/null)
+key=e5dd6da93c0517d413941ef0ba864c8f353e82d7
 ansible-galaxy collection build --output-path galaxy
-ansible-galaxy collection publish --api-key="$1" $(ls galaxy/*)
+ansible-galaxy collection publish --api-key="$key" $(ls galaxy/*)
